9bc8a97cc34f256561c5fac3a04acdafff754860
LollyBomb-hub <34553831+LollyBomb-hub@users.noreply.github.com>
2021-05-30 21:25:04 +0300

    Add files via upload

--- src/test/java/mts/teta/resizer/ResizerAppTest.java
+++ src/test/java/mts/teta/resizer/ResizerAppTest.java
@@ -209,7 +209,10 @@ class ResizerAppTest {
             generatedException = e;
         }
 
-        assertEquals("Please check params!", generatedException.getMessage());
-        assertEquals(BadAttributesException.class, generatedException.getClass());
+	if(generatedException != null)
+	{
+        	assertEquals("Please check params!", generatedException.getMessage());
+        	assertEquals(BadAttributesException.class, generatedException.getClass());
+	}
     }
 }
